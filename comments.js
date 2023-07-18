782cfd26273be18cd194eed5f8b10bf7ecd571c7
Adam Tong <adamtong@users.noreply.github.com>
2023-07-18 22:19:57 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,77 +1,26 @@
-// create web server
+// Create web server
+// 1. Create web server
+// 2. Create route
+// 3. Create controller
+// 4. Create view
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
-const cors = require('cors');
+const comments = require('./comments');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// create comments object
-const commentsByPostId = {};
-
-// handle event from event-bus
-app.post('/events', async (req, res) => {
-    const { type, data } = req.body;
-
-    if (type === 'CommentCreated') {
-        const { id, content, postId } = data;
-
-        // create comments array if postId not exists
-        const comments = commentsByPostId[postId] || [];
-        comments.push({ id, content, status: 'pending' });
-        commentsByPostId[postId] = comments;
-
-        // send event to event-bus
-        await axios.post('http://event-bus-srv:4005/events', {
-            type: 'CommentModerated',
-            data: {
-                id,
-                content,
-                postId,
-                status: 'pending'
-            }
-        });
-    }
-
-    if (type === 'CommentUpdated') {
-        const { id, content, postId, status } = data;
-
-        // find comments array
-        const comments = commentsByPostId[postId];
-        const comment = comments.find(comment => {
-            return comment.id === id;
-        });
-
-        // update status
-        comment.status = status;
-
-        // send event to event-bus
-        await axios.post('http://event-bus-srv:4005/events', {
-            type: 'CommentUpdated',
-            data: {
-                id,
-                content,
-                postId,
-                status
-            }
-        });
-    }
-
-    res.send({});
+app.get('/', (req, res) => {
+  res.send(comments.getComments());
 });
 
-// get all comments by postId
-app.get('/posts/:id/comments', (req, res) => {
-    const { id } = req.params;
-    const comments = commentsByPostId[id] || [];
-
-    res.send(comments);
+app.post('/add', (req, res) => {
+  const { comment } = req.body;
+  comments.addComment(comment);
+  res.redirect('/');
 });
 
-// listen for requests
-app.listen(4001, () => {
-    console.log('Listening on 4001');
+app.listen(3000, () => {
+  console.log('Web server is running on port 3000');
 });
-
